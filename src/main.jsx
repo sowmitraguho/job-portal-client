566817559f8ed0a70b389cfd96abb176f1cbe810
Sowmitra Guha <sowmmochy@gmail.com>
2025-05-27 21:23:18 +0600

    basic setup and installation done

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import router from './router/router.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
